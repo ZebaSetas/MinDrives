87034ef5e06d20f8b0696a718e0f42eaf73b1cbe
ZoiZeba <sebastianzawrzykraj@gmail.com>
2020-01-05 12:00:58 -0300

    the project was created

---
+++ MinDrives/MinDriveTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MinDriveTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MinDriveTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61e49d50-3695-4372-980c-f27556b2194e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MinDrives/Strategies/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Strategies
+{
+    public class Class1
+    {
+    }
+}
