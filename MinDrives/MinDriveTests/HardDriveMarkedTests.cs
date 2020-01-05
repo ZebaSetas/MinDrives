c699e870a24200a69fcd913bcfb70b7090b32689
ZoiZeba <sebastianzawrzykraj@gmail.com>
2020-01-05 12:54:35 -0300

    create test CreateEntityWithDataTest in HardDriveAdapterTests [red]

---
+++ MinDrives/MinDriveTests/HardDriveMarkedTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MinDriveTests
+{
+    [TestClass]
+    public class HardDriveMarkedTests
+    {
+        [TestMethod]
+        public void CreateEmptyEntityTest()
+        {
+            HardDriveMarked adapter = new HardDriveMarked();
+            Assert.AreEqual(adapter.HardDrive, null);
+            Assert.AreEqual(adapter.IsMarked, false);
+        }
+
+    }
+}
