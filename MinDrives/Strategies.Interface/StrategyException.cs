0b90dad5b2ec0eeafbc177b6d927f9b608ca9f70
ZoiZeba <sebastianzawrzykraj@gmail.com>
2020-01-05 13:32:59 -0300

    [green]

---
+++ MinDrives/Strategies.Interface/StrategyException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Strategies.Interface
+{
+    public class StrategyException : Exception
+    {
+      
+        public StrategyException(string message) : base(message)
+        {
+        }      
+    }
+}
--- MinDrives/Strategies/BacktrackingCalculatorMinDrives.cs
+++ MinDrives/Strategies/BacktrackingCalculatorMinDrives.cs
@@ -1,8 +1,16 @@
-﻿using System;
+﻿using Domain;
+using Strategies.Interface;
+using System;
+using System.Collections.Generic;
 
 namespace Strategies
 {
     public class BacktrackingCalculatorMinDrives
     {
+
+        public int Calculate(List<HardDrive> hardDrives)
+        {
+            throw new StrategyException("Hola mundo");
+        }
     }
 }
