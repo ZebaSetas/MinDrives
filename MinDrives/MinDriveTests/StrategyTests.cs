7747b4c1efacd81acb008af13a5c374c62407e97
ZoiZeba <sebastianzawrzykraj@gmail.com>
2020-01-05 13:39:01 -0300

    CalculateWithOneHardDriveTest is created [red]

--- MinDrives/MinDriveTests/StrategyTests.cs
+++ MinDrives/MinDriveTests/StrategyTests.cs
@@ -32,5 +32,20 @@ namespace MinDriveTests
             BacktrackingCalculatorMinDrives calculator = new BacktrackingCalculatorMinDrives();
             int result = calculator.Calculate(new List<HardDrive>());
         }
+
+        [TestMethod]
+        public void CalculateWithOneHardDriveTest()
+        {
+            BacktrackingCalculatorMinDrives calculator = new BacktrackingCalculatorMinDrives();
+            HardDrive hardrive = new HardDrive()
+            {
+                MaxSpace = 10,
+                UsedSpace = 1
+            };
+            List<HardDrive> hardDrives = new List<HardDrive>();
+            hardDrives.Add(hardrive);
+            int result = calculator.Calculate(hardDrives);
+            Assert.AreEqual(result, 1);
+        }
     }
 }
