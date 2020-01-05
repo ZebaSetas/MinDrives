ae4825d10c00dc28df4a2c3062dfc60704e72ac2
ZoiZeba <sebastianzawrzykraj@gmail.com>
2020-01-05 12:56:31 -0300

    create class HardDriveMarked [green][refactor]

---
+++ MinDrives/Domain/HardDriveMarked.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class HardDriveMarked
+    {
+        public HardDrive HardDrive { get; set; }
+        public bool IsMarked { get; set; }
+    }
+}
