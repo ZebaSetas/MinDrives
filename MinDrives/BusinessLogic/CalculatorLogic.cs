2af1396507607dfe6aecd4a212ae79ea1f77263b
ZoiZeba <sebastianzawrzykraj@gmail.com>
2020-01-06 01:07:37 -0300

    CalculateWithOneInvalidHarDriveTest and CalculateWithInvalidHarDriveTwoTest was created [red]

--- MinDrives/BusinessLogic/CalculatorLogic.cs
+++ MinDrives/BusinessLogic/CalculatorLogic.cs
@@ -8,14 +8,15 @@ namespace BusinessLogic
         public int MinDrives(int[]used, int[]total)
         {
             ThrowExceptionIfIsInvalid(used,total);
-            throw new BusinessLogicException("");
+            return 0;
         }
 
         private void ThrowExceptionIfIsInvalid(int[] used, int[] total)
         {
             bool thereIsListNull = used == null || total == null;
             if(thereIsListNull) throw new BusinessLogicException("Error! List of used or total is null");
-
+            bool theListsAreNotSameSize = used.Length != total.Length;
+            if(theListsAreNotSameSize) throw new BusinessLogicException("Error! List of used or total are not the same size");
         }
     }
 }
